697b647c57c44b34d072259e47364081f00271df
Dave Lasley <dave@laslabs.com>
2016-12-14 14:00:12 -0800

    [REF] clouder_template_elasticsearch: PR Review updates

--- clouder_template_elasticsearch/__openerp__.py
+++ clouder_template_elasticsearch/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Clouder Template Elasticsearch',
@@ -9,6 +9,7 @@
     'depends': [
         'clouder',
         'clouder_template_proxy',
+        'clouder_template_dns',
     ],
     'author': 'LasLabs Inc.',
     'license': 'LGPL-3',
